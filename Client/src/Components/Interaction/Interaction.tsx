8e0062e75a66475e9ba7ba091223d5c322a8dff8
domi <domi@domi.kr>
2024-08-09 00:54:41 +0900

    💣 interaction add

--- Client/src/Components/Interaction/Interaction.tsx
+++ Client/src/Components/Interaction/Interaction.tsx
@@ -3,7 +3,32 @@ import style from './interaction.module.css';
 export default function InteractionPage() {
     return <main className={style.main}>
         <section className={style.row}>
-
+            <ButtonCountBox />
+            
+            <div className={style.line}></div>
+            
+            <SkillCountBox />
         </section>
     </main>;
 }
+
+function ButtonCountBox() {
+    return <section className={style.box}>
+        <h2>버튼 사용 현황</h2>
+        
+        <section className={style.list}>
+            
+        </section>
+    </section>;
+}
+
+function SkillCountBox() {
+    return <section className={style.box}>
+        <h2>스킬 사용 현황</h2>
+    
+        <section className={style.list}>
+
+        </section>
+    </section>;
+}
+
