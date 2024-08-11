ff7f3b0b5ca019113cbb12e67dba293e40b7d619
domi <domi@domi.kr>
2024-08-11 18:47:27 +0900

    🕛 server scene chart

--- Server/admin/time.js
+++ Server/admin/time.js
@@ -11,6 +11,12 @@ app.get("/admin/time/ingame_chart", async function(req, res) {
     res.send(rows);
 });
 
-app.get("/admin/time/scene", async function(req, res) {
+app.get("/admin/time/scene_chart", async function(req, res) {
+    const YYMMDD = getYYMMDD_time(2); // 무조건 30일 전
     
+    const [rows] = await sql.query(`SELECT scene, FLOOR(AVG(time)) AS time, created FROM 
+        (SELECT UUID, scene, SUM(TIME) AS time, DATE(created) AS created FROM time_scene WHERE DATE(created) >= ? GROUP BY UUID, scene, DATE(created)) AS domi
+        GROUP BY scene, created;`, [ YYMMDD ]);
+
+    res.send(rows);
 });
