4ae2f67d10baa43b8ec6327b999d9bfb9e5fdff4
domi <domi@domi.kr>
2024-08-11 12:23:37 +0900

    ⏰ time scene api

--- Server/game/time.js
+++ Server/game/time.js
@@ -1,4 +1,5 @@
 const sql = require('../lib/sql.js');
+const scenes = require("../scene.json");
 
 function randomString(length) {
     return Math.random().toString(36).substr(2, length);
@@ -61,7 +62,45 @@ app.post("/game/time/ingame", async function(req, res) {
         res.send("ok!");
     } catch { // 실패
         res.sendStatus(500);
-        
+
+        await connection.rollback();
+    }
+    
+    connection.release();
+});
+
+const indexScene = {};
+scenes.forEach(v => indexScene[v] = true);
+
+app.post("/game/time/scene", async function(req, res) {
+    const { scene, token } = req.body;
+
+    if (typeof token !== "string") {
+        return res.sendStatus(400);
+    }
+    if (typeof scene !== "string" || indexScene[scene] !== true) {
+        return res.status(400).send("알 수 없는 scene");
+    }
+    
+    const connection = await sql.getConnection();
+    await connection.beginTransaction(); // 트랜잭션 시작
+    
+    // 걸린시간 불러오기
+    const duration = await getDuration(connection, req.uuid, token);
+    if (duration === false) {
+        connection.release();
+        res.status(400).send("not found token");
+        return;
+    }
+
+    try {
+        await connection.query("INSERT INTO time_scene VALUES(?, ?, ?, NOW())", [ req.uuid, scene, duration ]);
+        await connection.commit();
+
+        res.send("ok!");
+    } catch { // 실패
+        res.sendStatus(500);
+
         await connection.rollback();
     }
     
